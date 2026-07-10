5cd75271e15366c8b079250f0c18a6c25f07a4ba
Nishtha Jaiswal <182102744+nishh-tha@users.noreply.github.com>
2026-07-10 14:26:58 +0530

    Added JUnit Exercises 1, 3 and 4

---
+++ TDD-Basic Testing/junit-handson/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+
+    }
+}
---
+++ TDD-Basic Testing/junit-handson/src/test/java/com/example/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testSetup() {
+        System.out.println("JUnit setup successful!");
+    }
+
+}
