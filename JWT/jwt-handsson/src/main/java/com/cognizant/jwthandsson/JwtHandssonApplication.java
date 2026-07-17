0b86134db09860d9e9707f05bdd2501dfb0a4b22
Nishtha Jaiswal <182102744+nishh-tha@users.noreply.github.com>
2026-07-17 22:30:28 +0530

    Implement JWT authentication service

---
+++ JWT/jwt-handsson/src/main/java/com/cognizant/jwthandsson/JwtHandssonApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.jwthandsson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtHandssonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtHandssonApplication.class, args);
+	}
+
+}
---
+++ JWT/jwt-handsson/src/test/java/com/cognizant/jwthandsson/JwtHandssonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.jwthandsson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtHandssonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
